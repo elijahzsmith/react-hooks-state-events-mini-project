5939d97ef65e3da5927067a9aefaf89ef7ca6c7a
Elijah Smith <elijahsmith.m@gmail.com>
2022-12-05 19:47:37 -0600

    Comment cleanup

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,28 +1,21 @@
 import React from "react";
-import Task from './Task'
+import Task from "./Task";
 
 function TaskList({ handleDelete, inTaskList }) {
-  //console.group(inTaskList)
-  //console.group(handleDelete)
+  console.log(inTaskList);
+  if (!inTaskList) {
+    return <h1>Waiting</h1>;
+  }
+  const displayTasks = inTaskList.map((task) => (
+    <Task
+      onDelete={() => handleDelete(task.text)}
+      key={task.text}
+      text={task.text}
+      category={task.category}
+    />
+  ));
 
-  // MAKES EACH TASK
-  // Variable that holds the map that creates each new tax item to be rendered in the return.
-  const displayTasks = inTaskList.map(task => ( <Task 
-   onDelete={() => handleDelete(task.text)}
-   key={task.text}
-   className='All' ////---- EXPIREMENT__use_this_to_show_all_ON-START ----////
-   text={task.text} 
-   category={task.category}
-   /> 
-   ))
-
-  // Returns the JSX that makes up the list of tasks
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-       {displayTasks}
-    </div>
-  );
+  return <div className="tasks">{displayTasks}</div>;
 }
 
 export default TaskList;
